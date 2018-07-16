558d9b9b5b31d1ee3a6a3384d4a81ea5132239fa
Hadar Pur <hpur@liveperson.com>
2018-07-16 23:24:19 +0300

    Initial Commit

---
+++ HW1_HadarPur/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HW1_HadarPur
+//
+//  Created by Hadar Pur on 16/07/2018.
+//  Copyright © 2018 Hadar Pur. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
